77b61eab2fa5c8f78daf74f213146ea5ce73aa7b
MacBook <macbook@MacBooks-MacBook-Pro-7.local>
2020-04-21 13:54:19 +0700

    nodejs dasar

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router()
+const path = require('path');
+
+router.get('/add-product',(req, res, next) =>{
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/product',(req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/')
+});
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
